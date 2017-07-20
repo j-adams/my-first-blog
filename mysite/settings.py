8ad3686d127cde8d7aa6c745d35cb5ae00483a55
jenniferadams <jenniferadams028@gmail.com>
2017-07-20 11:07:46 -0400

    Access changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '53b!x(75juo*a9mf3=9os8r^6-zdga&_20wm_@gj6b@r4%$v(g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','jenniferadams.pythonanywhere.com']
 
 
 # Application definition
